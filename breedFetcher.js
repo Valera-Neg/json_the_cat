f39c7319f1e5784c10ba684c9fda94e0dc2a346e
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-18 13:17:17 +0000

    breet fatcher from TheCatAPI

---
+++ breedFetcher.js
@@ -0,0 +1,10 @@
+const request = require('request');
+const breedFetcher = function(breed) {
+  if (!breed) throw new Error('Breed is not defined');
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+      if (error) throw new Error('unsaccessful request');
+        const data = JSON.parse(body);
+        console.log(data[0].description);
+  });
+};
+breedFetcher(process.argv[2]);
