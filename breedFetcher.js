382abe81264769cfd865040ed35057a35058793a
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-18 14:18:00 +0000

    better

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,8 +3,10 @@ const breedFetcher = function(breed) {
   if (!breed) throw new Error('Breed is not defined');
     request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
       if (error) throw new Error('unsaccessful request');
-        const data = JSON.parse(body);
-        console.log(data[0].description);
+      const data = JSON.parse(body);
+      console.log(data);
+      if (!data.length) throw new Error('breed not found');
+      console.log(data[0].description);
   });
 };
 breedFetcher(process.argv[2]);
